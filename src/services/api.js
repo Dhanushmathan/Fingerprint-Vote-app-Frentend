6cb6aa19b2b2a9074e2ce9f071c0a0e19e7aa71c
Dhanushmathan <mathan2394>
2026-03-27 15:12:54 +0530

    fix: vercel routing

--- src/services/api.js
+++ src/services/api.js
@@ -8,6 +8,9 @@ const api = axios.create({
     headers: { 'Content-Type': 'application/json' },
 });
 
+console.log(import.meta.env.VITE_API_URL, 'API URL');
+
+
 // Response interceptor - handle errors globally
 api.interceptors.response.use(
     res => res,
